6e3cfc937dc48fc96c9c406a4471d6ef1d22af7d
asiuzzaman006 <zaman1304006@gmail.com>
2020-02-01 23:36:30 +0600

    just create a node

--- insertInLinkList.cpp
+++ insertInLinkList.cpp
@@ -6,9 +6,18 @@ struct Node{
     Node * next;
 };
 
+Node * creatNode(int data){
+    Node * newNode=(Node*) malloc(sizeof(Node));
+    newNode->data=data;
+    newNode->next=NULL;
+
+    return newNode;
+}
 
 
 int main(){
    printf("Program Initialize....\n");
-   
+   Node * Root=creatNode(5);
+   printf("%d \n",Root->data);
+
 }
